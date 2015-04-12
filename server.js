ff95b271bcd28b4857674c75803f7baaaac4da09
us3soap <kevin.valette@gmail.com>
2015-04-12 18:22:24 +0200

    Mise en place du livrable

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
-
 var qr = require('qr-image');
+var room = require('room-module');
 
 // Creates a new instance of SimpleServer with the following options:
 //  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
@@ -13,74 +13,17 @@ var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
 //Génération du flux correspondant à l'image du QR Code
-router.get('/qr-code', function(req, res) {  
-  //On affiche l'url du site
-  var code = qr.image(req.protocol+'://'+req.headers.host, { type: 'svg' });
-  res.type('svg');
-  code.pipe(res);
-  console.log('qr-code affiché :'+req.protocol+'://'+req.headers.host);
+router.get('/', function(req, res) {
+    //On affiche l'url du site
+    var code = qr.image(req.protocol+'://'+req.headers.host, { type: 'svg' });
+    res.type('svg');
+    code.pipe(res);
+    console.log('qr-code affiché :'+req.protocol+'://'+req.headers.host);
 });
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
 
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+server.listen(process.env.PORT || 3010, process.env.IP || "0.0.0.0", function(){
+    var addr = server.address();
+    console.log("QuizzJS run to : [", addr.address + ":" + addr.port+"]");
 });
